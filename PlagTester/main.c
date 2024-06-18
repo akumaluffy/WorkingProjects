2a0d31a408c024de6caa28c02fa6359ce9eb307c
akumaluffy <yliu0602@gmail.com>
2024-06-18 15:35:14 -0500

    create initial working file and plan steps

---
+++ PlagTester/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ------- Takes two files and compares for plagiarism ------- // 
+
+/* 
+1. Files are .c files, convert to .txt files and compare (?)
+2. Files should come from a directory containing all files
+3. Process places file path of all files in directory into a string array (or maybe struct?)
+4. Compares each file with all other files
+5. Compare each line in file 1 with all other lines?? (this might be way too much)
+6. Maybe make each file comparison a separate process and use a process manager like in HW4
+*/
+
+int main(int argc, char **argv) {
+
+    return 0;
+}
